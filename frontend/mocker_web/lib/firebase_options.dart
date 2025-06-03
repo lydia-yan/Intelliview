af1b2fcfd47de3a80b4980192090d6159d5109ba
Moyan <linmoyan0111@gmail.com>
2025-06-03 00:14:33 -0700

    fix: resolve Python 3.13 compatibility issues with Google ADK dependencies

--- frontend/mocker_web/lib/firebase_options.dart
+++ frontend/mocker_web/lib/firebase_options.dart
@@ -14,12 +14,12 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIxxxxxxAlBxxxebVO8Ck',
-    appId: '1:346xxx29:web:67xxx3c6xxxa8',
-    messagingSenderId: '3xx9xxx2xxxxx9',
+    apiKey: 'AIzaSyAlBCWpQhnOxo8j9GNqv6OBebVSZZBO8Ck',
+    appId: '1:346901812089:web:44b67dd6b2d43c64b9d5a8',
+    messagingSenderId: '346901812089',
     projectId: 'mock-interview-app-2025',
     authDomain: 'mock-interview-app-2025.firebaseapp.com',
     storageBucket: 'mock-interview-app-2025.firebasestorage.app',
-    measurementId: 'G-xxxxxQxxxD6',
+    measurementId: 'G-RY4Q7B46D6',
   );
 }
